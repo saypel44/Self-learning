c9dafd0d126c202847f4e47579892d89c310459a
03230293 <03230293.gcbs@rub.edu.bt>
2025-05-01 23:16:04 +0600

    Using of main guard in program/ how to call the defined function

---
+++ 2main_guard.py
@@ -0,0 +1,9 @@
+#use of f __name__ == "__main__":):
+# Runs only if the script is executed directly for the define function
+
+
+def greet():
+    print("Kuzu zangpo la!")
+
+if __name__== "__main__":
+    greet()
